b85b7b5fde27ae22642d322fe3fc48a44e66780f
(YongSun.Lee) <(yongsun.lee.8802@gmail.com)>
2024-03-01 16:56:29 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React And How To Use Git-Hub
         </a>
       </header>
     </div>
